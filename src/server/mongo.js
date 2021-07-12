763f0d3875e5bd435eaa1eaf9726b64eb7484d06
Atakan Unal <atakanunal25@gmail.com>
2021-07-12 23:29:42 +0300

    Debug: Heroku CI

--- src/server/mongo.js
+++ src/server/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-console.log(process.env.MONGO_URI);
+console.log(process.env.BASE_URL);
 
 mongoose.connect(process.env.MONGO_URI, { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
