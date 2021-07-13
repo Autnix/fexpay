934f30a346a61b1af0912a2908f5c98870d28aad
Atakan Unal <atakanunal25@gmail.com>
2021-07-13 12:51:22 +0300

    Debug: Heroku CI

--- config/env.js
+++ config/env.js
@@ -1,3 +1,5 @@
+console.log("ENV", process.env.MONGO_URI);
+
 module.exports = {
   MONGO_URI: process.env.MONGO_URI
 };
--- src/server/mongo.js
+++ src/server/mongo.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const APP_ENV = require('../../config/env');
 
+console.log("MODULE:MONGO", process.env.MONGO_URI);
+
 mongoose.connect(APP_ENV.MONGO_URI, { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
     console.log("🚀 MongoDB connected!");
