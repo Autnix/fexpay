35b523c550d3541a3e4a4c0105783cbca6883ca8
Atakan Unal <atakanunal25@gmail.com>
2021-08-15 19:05:00 +0300

    Add: Redis Connection Logs

--- src/server/redis.js
+++ src/server/redis.js
@@ -1,10 +1,18 @@
-const Redis = require('ioredis');
-const cluster = new Redis(process.env.REDIS_URI || "127.0.0.1:6379")
+const Redis = require('ioredis')
+const cluster = new Redis(process.env.REDIS_URI || '127.0.0.1:6379')
 
 // .then(() => {
 //   console.log("📡 Redis connected!")
 // }).catch(err => {
 //   console.log(`❌ Redis Connection Failed: ${err}`);
 // })
+
+cluster.on('connect', () => {
+  console.warn('📡 Redis connected!')
+})
+
+cluster.on('error', err => {
+  console.error(`❌ Redis Connection Failed: ${err}`)
+})
 
 module.exports = cluster
