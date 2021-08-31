07914d9e0f5cdc8b315b3a9b3765434b433acd2a
Atakan Unal <atakanunal25@gmail.com>
2021-08-31 17:40:43 +0300

    Hotfix: Font Styles

--- config/head.js
+++ config/head.js
@@ -7,6 +7,8 @@ module.exports = {
         { hid: 'description', name: 'description', content: '' }
     ],
     link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;400;500;600;700;800;900&display=swap' },
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
 };
